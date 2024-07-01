91928d97924efb34fd9e45adf1837f3071daa8a1
Hector Bjernersjö <hector@bjernersjo.se>
2024-07-01 10:22:36 +0000

    fixed install script, starship and fonts

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,18 +2,20 @@
 
 # Install script for everything
 user=$(whoami)
+current=$(pwd)
 
 # Install yay for aur
 git clone https://aur.archlinux.org/yay.git $HOME/yay
 cd $HOME/yay && makepkg -si
+# cd $HOME/dotfiles
 
 # Install pacman packages
-./install_pacman.sh pacman_packages.txt
+$current/install_pacman.sh $current/pacman_packages.txt
 
 # Install aur packages
-./install_yay.sh yay_packages.txt
+$current/install_yay.sh $current/yay_packages.txt
 
-./tmux-setup.sh
+$current/tmux-setup.sh
 
 # Set config files
 stow $HOME/dotfiles
