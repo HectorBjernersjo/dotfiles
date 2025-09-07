e41476bab30d87deb97e2c612459bd1a6ff6a5aa
Hector Bjernersjö <hector@bjernersjo.se>
2025-09-07 22:03:58 +0200

    better setup script

--- installation/setup.sh
+++ installation/setup.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+mkdir -p "$HOME/.config"
+mkdir -p "$HOME/.ssh"
+stow -d "$HOME/dotfiles" .
